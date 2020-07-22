add  rectification of stereo and calculation of b, fix large RMS problem for stereo calibration

scripts/calibration_undistort.py
@@ -24,17 +24,18 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 存储棋盘点的3D世界坐标,为棋盘点坐标
 imgpoints = [] # 2D图像坐标,利用cv2的API求得
 
-images = glob.glob('../imgs/left/*.jpg')
+images = sorted(glob.glob('../imgs/left/*.jpg'))
 
-for fname in images:
+for i,fname in enumerate(images):
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img_size = gray.shape[::-1]     #　图像尺寸为(w,h)
 
     # 利用cv的API找到棋盘点在图像中的2D坐标
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
 
     # 如果找到了棋盘点,将该场景下的3D和2D坐标加入到存储中
+    print(i, ret)
     if ret:
         objpoints.append(objp)
 
@@ -62,13 +63,13 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_siz
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,img_size,1,img_size)
 
 # 利用相机矩阵进行畸变校正
-img = cv2.imread('../imgs/left/left01.jpg')
+img = cv2.imread('../imgs/right/right01.jpg')
 dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
 # 裁剪图像
 x,y,w,h = roi
 dst = dst[y:y+h, x:x+w]
-cv2.imwrite('../imgs/undistort/after_undistort.jpg',dst)
+cv2.imwrite('../imgs/undistort/after_undistort_right.jpg',dst)
 # cv2.imshow("img", dst)
 # cv2.waitKey(5000)
 cv2.destroyAllWindows()

scripts/stereo_calibration.py
@@ -13,9 +13,10 @@
 import numpy as np
 import cv2
 import glob
+import os
 
 # 迭代停止标准
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-5)
 
 # 准备建系在棋盘上的棋盘角点的坐标,Z坐标全部为0
 objp = np.zeros((6*7,3), np.float32)
@@ -29,8 +30,20 @@ right_imgpoints = [] # 2D图像坐标,利用cv2的API求得
 left_imgs = glob.glob('../imgs/left/*.jpg')
 right_imgs = glob.glob('../imgs/right/*.jpg')
 
+left_imgs = sorted(left_imgs)
+right_imgs = sorted(right_imgs)
+
+os.system('rm -rf ../imgs/check/*')
+
+# 某些pair会导致过大的RMS，将这些pair去除
+excluded_idx = [12]
+
 print('收集图像点坐标...')
-for left,right in zip(left_imgs,right_imgs):
+for i,(left,right) in enumerate(zip(left_imgs,right_imgs)):
+    if i in excluded_idx:
+        continue
+
+    print(left,right)
     l_img = cv2.imread(left)
     r_img = cv2.imread(right)
 
@@ -66,9 +79,20 @@ for left,right in zip(left_imgs,right_imgs):
             right_imgpoints.append(r_corners)
         # --------------------------------------------------------------------------#
 
+        l_tagged_img = cv2.drawChessboardCorners(l_img, (7,6), l_corners2, l_ret)
+        r_tagged_img = cv2.drawChessboardCorners(r_img, (7,6), r_corners2, r_ret)
+        # cv2.imshow('img_left',l_tagged_img)
+        # cv2.imshow('img_right',r_tagged_img)
+        # cv2.waitKey(2000)
+        cv2.imwrite(f'../imgs/check/{i+1}_l.jpg', l_tagged_img)
+        cv2.imwrite(f'../imgs/check/{i + 1}_r.jpg', r_tagged_img)
+
+
 print('逐个标定单目摄像机...')
-ret, l_mtx, l_dist, l_rvecs, l_tvecs = cv2.calibrateCamera(objpoints, left_imgpoints, l_img_size, None, None)
-ret, r_mtx, r_dist, r_rvecs, r_tvecs = cv2.calibrateCamera(objpoints, right_imgpoints, r_img_size, None, None)
+l_ret, l_mtx, l_dist, l_rvecs, l_tvecs = cv2.calibrateCamera(objpoints, left_imgpoints, l_img_size, None, None, criteria=criteria)
+r_ret, r_mtx, r_dist, r_rvecs, r_tvecs = cv2.calibrateCamera(objpoints, right_imgpoints, r_img_size, None, None, criteria=criteria)
+# print('左Camera标定的RMS:', l_ret)
+# print('右Camera标定的RMS:', r_ret)
 
 print('标定双目摄像机...')
 retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = \
@@ -79,14 +103,15 @@ retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = \
                         l_dist,
                         r_mtx,
                         r_dist,
-                        l_img_size)
+                        l_img_size,
+                        criteria=criteria)
 
 print('*'*50)
 print('左摄像机到右摄像机坐标系的旋转矩阵:','\n',R)
 print('左摄像机到右摄像机坐标系的位移矩阵:','\n',T)
 print('左摄像机到右摄像机坐标系的本征矩阵:','\n',E)
 print('左摄像机到右摄像机坐标系的基础矩阵:','\n',F)
+print('重建损失:', retval)
 
-
-
-
+cv2.destroyAllWindows()
+#